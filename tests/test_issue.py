35b315e9e5fee8e5eeec81449f1daee5c7db0f9f
hassaku63 <takuyahashimoto1988@gmail.com>
2018-10-16 01:01:58 +0900

    issue function

---
+++ tests/test_issue.py
@@ -0,0 +1,60 @@
+from unittest import TestCase
+
+
+class TestIssue(TestCase):
+    def test_list(self):
+        self.fail()
+
+    def test_count(self):
+        self.fail()
+
+    def test_create(self):
+        self.fail()
+
+    def test_get(self):
+        self.fail()
+
+    def test_update(self):
+        self.fail()
+
+    def test_delete(self):
+        self.fail()
+
+    def test_get_comments(self):
+        self.fail()
+
+    def test_add_comment(self):
+        self.fail()
+
+    def test_count_comments(self):
+        self.fail()
+
+    def test_get_comment(self):
+        self.fail()
+
+    def test_update_comment(self):
+        self.fail()
+
+    def test_get_comment_notifications(self):
+        self.fail()
+
+    def test_add_comment_notifycation(self):
+        self.fail()
+
+    def test_list_issue_attachments(self):
+        self.fail()
+
+    def test_get_issue_attachments(self):
+        self.fail()
+
+    def test_delete_issue_attachment(self):
+        self.fail()
+
+    def test_list_issue_shared_files(self):
+        self.fail()
+
+    def test_link_issue_shared_files(self):
+        self.fail()
+
+    def test_unlink_issue_shared_file(self):
+        self.fail()
