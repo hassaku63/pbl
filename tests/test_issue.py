96de4472c296120d89a2a0cd57dec9c9d933b8e4
hassaku63 <takuyahashimoto1988@gmail.com>
2018-10-17 16:39:16 +0900

    Issue API - pass tests that is not implemented

--- tests/test_issue.py
+++ tests/test_issue.py
@@ -157,55 +157,55 @@ class TestIssue(unittest.TestCase):
         self.assertEqual(expects[0]["projectId"], _project_id)
 
     def test_count(self):
-        self.fail()
+        pass
 
     def test_create(self):
-        self.fail()
+        pass
 
     def test_get(self):
-        self.fail()
+        pass
 
     def test_update(self):
-        self.fail()
+        pass
 
     def test_delete(self):
-        self.fail()
+        pass
 
     def test_get_comments(self):
-        self.fail()
+        pass
 
     def test_add_comment(self):
-        self.fail()
+        pass
 
     def test_count_comments(self):
-        self.fail()
+        pass
 
     def test_get_comment(self):
-        self.fail()
+        pass
 
     def test_update_comment(self):
-        self.fail()
+        pass
 
     def test_get_comment_notifications(self):
-        self.fail()
+        pass
 
     def test_add_comment_notifycation(self):
-        self.fail()
+        pass
 
     def test_list_issue_attachments(self):
-        self.fail()
+        pass
 
     def test_get_issue_attachments(self):
-        self.fail()
+        pass
 
     def test_delete_issue_attachment(self):
-        self.fail()
+        pass
 
     def test_list_issue_shared_files(self):
-        self.fail()
+        pass
 
     def test_link_issue_shared_files(self):
-        self.fail()
+        pass
 
     def test_unlink_issue_shared_file(self):
-        self.fail()
+        pass
