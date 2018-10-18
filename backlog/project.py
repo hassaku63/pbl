72793a2696d6244ee5fb8f7fe9261cb2d12a66b0
hassaku63 <takuyahashimoto1988@gmail.com>
2018-10-18 22:21:04 +0900

    [feat] project: issue type api

--- backlog/project.py
+++ backlog/project.py
@@ -79,3 +79,87 @@ class Project(object):
         resp = self.api.invoke_method(_method, _uri, query_param=_query_param)
 
         return resp.json()
+
+    def list_issue_types(self, projectIdOrKey):
+        """
+        https://developer.nulab-inc.com/ja/docs/backlog/api/2/get-issue-type-list/
+
+        :param projectIdOrKey:
+        :return:
+        """
+        _uri = "projects/{project_id_or_key}/issueTypes".format(
+            project_id_or_key=projectIdOrKey
+        )
+        _method = "GET"
+
+        resp = self.api.invoke_method(_method, _uri)
+
+        return resp.json()
+
+    def add_issue_type(self, projectIdOrKey, name, color):
+        """
+        https://developer.nulab-inc.com/ja/docs/backlog/api/2/add-issue-type/
+
+        :param projectIdOrKey:
+        :param name:
+        :param color:
+        :return:
+        """
+        _uri = "projects/{project_id_or_key}/issueTypes".format(
+            project_id_or_key=projectIdOrKey
+        )
+        _method = "POST"
+        _request_param = {
+            "name": name,
+            "color": color
+        }
+
+        resp = self.api.invoke_method(_method, _uri, request_param=_request_param)
+
+        return resp.json()
+
+    def update_issue_type(self, projectIdOrKey, id, name, color):
+        """
+        https://developer.nulab-inc.com/ja/docs/backlog/api/2/update-issue-type/
+
+        :param projectIdOrKey:
+        :param id: int Issue type ID
+        :param name:
+        :param color:
+        :return:
+        """
+        _uri = "projects/{project_id_or_key}/issueTypes/{id}".format(
+            project_id_or_key=projectIdOrKey,
+            id=id
+        )
+        _method = "PATCH"
+        _request_param = {
+            "name": name,
+            "color": color
+        }
+
+        resp = self.api.invoke_method(_method, _uri, request_param=_request_param)
+
+        return resp.json()
+
+    def delete_issue_type(self, projectIdOrKey, id, substituteIssueTypeId):
+        """
+        https://developer.nulab-inc.com/ja/docs/backlog/api/2/delete-issue-type/
+
+        :param projectIdOrKey: Project ID or Key
+        :param id: Issue Type ID
+        :param substituteIssueTypeId: New Issue type ID
+        :return:
+        """
+        _uri = "projects/{project_id_or_key}/issueTypes/{id}".format(
+            project_id_or_key=projectIdOrKey,
+            id=id
+        )
+        _method = "DELETE"
+        _request_param = {
+            "substituteIssueTypeId": substituteIssueTypeId
+        }
+
+        resp = self.api.invoke_method(_method, _uri, request_param=_request_param)
+
+        return resp.json()
--- tests/test_project.py
+++ tests/test_project.py
@@ -83,6 +83,131 @@ class TestProject(unittest.TestCase):
     def test_delete_user(self):
         pass
 
+    @httpretty.activate
+    def test_list_types(self):
+        _project_id = 1
+        _uri = "projects/{project_id_or_key}/issueTypes".format(
+            project_id_or_key=_project_id
+        )
+        expects = [
+            {
+                "id": 1,
+                "projectId": _project_id,
+                "name": "バグ",
+                "color": "#990000",
+                "displayOrder": 0
+            }
+        ]
+
+        httpretty.register_uri(
+            httpretty.GET,
+            API_ENDPOINT.format(space=self.space, uri=_uri),
+            body=json.dumps(expects)
+        )
+
+        resp = self.api.project.list_issue_types(projectIdOrKey=_project_id)
+
+        self.assertEqual(list, type(resp))
+        self.assertEqual(expects[0]["name"], resp[0]["name"])
+
+    @httpretty.activate
+    def test_add_issue_type(self):
+        _project_id = 1
+        _name = "バグ"
+        _color = "#990000"
+        _uri = "projects/{project_id_or_key}/issueTypes".format(
+            project_id_or_key=_project_id
+        )
+        expects = {
+            "id": 1,
+            "projectId": _project_id,
+            "name": _name,
+            "color": _color,
+            "displayOrder": 0
+        }
+
+        httpretty.register_uri(
+            httpretty.POST,
+            API_ENDPOINT.format(space=self.space, uri=_uri),
+            body=json.dumps(expects)
+        )
+
+        resp = self.api.project.add_issue_type(
+            projectIdOrKey=_project_id,
+            name=_name,
+            color=_color
+        )
+
+        self.assertEqual(expects["projectId"], resp["projectId"])
+        self.assertEqual(expects["name"], resp["name"])
+
+    @httpretty.activate
+    def test_update_issue_type(self):
+        _project_id = 1
+        _id = 1
+        _name = "バグ"
+        _color = "#990000"
+        _uri = "projects/{project_id_or_key}/issueTypes/{id}".format(
+            project_id_or_key=_project_id,
+            id=_id
+        )
+        expects = {
+            "id": _id,
+            "projectId": _project_id,
+            "name": _name,
+            "color": _color,
+            "displayOrder": 0
+        }
+
+        httpretty.register_uri(
+            httpretty.PATCH,
+            API_ENDPOINT.format(space=self.space, uri=_uri),
+            body=json.dumps(expects)
+        )
+
+        resp = self.api.project.update_issue_type(
+            projectIdOrKey=_project_id,
+            id=_id,
+            name=_name,
+            color=_color
+        )
+
+        self.assertEqual(expects["projectId"], resp["projectId"])
+        self.assertEqual(expects["name"], resp["name"])
+        self.assertEqual(expects["color"], resp["color"])
+
+    @httpretty.activate
+    def test_delete_issue_type(self):
+        _project_id = 1
+        _id = 1
+        _substituteIssueTypeId = 2
+        _uri = "projects/{project_id_or_key}/issueTypes/{id}".format(
+            project_id_or_key=_project_id,
+            id=_id
+        )
+        expects = {
+            "id": _id,
+            "projectId": _project_id,
+            "name": "バグ",
+            "color": "#990000",
+            "displayOrder": 0
+        }
+
+        httpretty.register_uri(
+            httpretty.DELETE,
+            API_ENDPOINT.format(space=self.space, uri=_uri),
+            body=json.dumps(expects)
+        )
+
+        resp = self.api.project.delete_issue_type(
+            projectIdOrKey=_project_id,
+            id=_id,
+            substituteIssueTypeId=_substituteIssueTypeId
+        )
+
+        self.assertEqual(expects["projectId"], resp["projectId"])
+        self.assertEqual(expects["name"], resp["name"])
+
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
